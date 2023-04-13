1626b7f7c9eaa513a09174d181bd47533369a500
Marc <marcpuertom97@gmail.com>
2023-04-13 12:36:55 +0200

    api request and projec cnstruct

---
+++ script.js
@@ -0,0 +1,49 @@
+const img_weather = document.getElementById("weather-icon");
+const country = document.getElementsByClassName("country");
+const city = document.getElementsByClassName("city");
+const temp = document.getElementsByClassName("temp");
+const localtime = document.getElementsByClassName("localtime");
+
+
+async function getWeather(location) {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=0e00de01a4854ff885784323231304&q=${location}`,
+    {
+      mode: "cors",
+    }
+  );
+
+  if (response.status === 400) {
+    throwErrorMsg();
+  } else {
+    const weatherData = await response.json();
+    let data = bindData(weatherData);
+    displayData(data);
+  }
+}
+
+function bindData(weatherData){
+
+    const webData = {
+
+        country: weatherData.location.country,
+        name: weatherData.location.name,
+        localtime: weatherData.location.localtime,
+        temp: weatherData.current.temp_c,
+        temp_img: weatherData.current.condition.icon
+    }
+
+    return webData;
+}
+
+
+function displayData(webData){
+    console.log(webData)
+    country[0].textContent = webData.country
+    city[0].textContent = webData.name
+    temp[0].textContent = webData.temp
+    localtime[0].textContent = webData.localtime
+    img_weather.setAttribute("src", 'http://' + webData.temp_img);
+}
+
+getWeather("London");
